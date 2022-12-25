95a38fc0fb1fd82f40af3c85b1ef731205b415fb
irfan36bot <116106429+irfan36bot@users.noreply.github.com>
2022-12-25 22:18:14 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=16784506, cast=int)
-API_HASH = config("API_HASH", default=562689b74bbd77c6baeceb097204c191)
-BOT_TOKEN = config("BOT_TOKEN", default=5810976226:AAHMZDOVu5KBapO__fTBOd5bZ-TknOXJoRU)
-SESSION = config("SESSION", default=BQEO_1IAbkm1MUN3BccUYgDelw4oLgYa6w5bLmH5VRWZhRI41UX2BB11gAAuA1wGp3IjPvgAdc_SiC_8lAxG7llQ30xbt-mgCoy2L3KXXWJZz4RR8X-0gDLpNS170t944E6Eub3NFy3y_Vd1iISCGNn36X5_2Qjd-oK6wzYuoLFHcnLSFSWBOGnusPRUgI3YF7tQ7g0M36pzXeHNCf7Cw6NAVNyhDi4yZgKC6Wbd-97xxvbU_YmiqiOAa7Ee6QtBklt2hiNee_TxO5ISPcFgwGCyjhgu61xGiuNNELUtUzjiIUP84DFZyeVJq1CkWwk8iQCKj2dSbOwCLajA5mX7-pRzyYzk9QAAAAE_AG8rAA)
-FORCESUB = config("FORCESUB", default=DS_Botz)
-AUTH = config("AUTH", default=1125671241, cast=int)
+API_ID = 16784506
+API_HASH = "562689b74bbd77c6baeceb097204c191"
+BOT_TOKEN = "5810976226:AAHMZDOVu5KBapO__fTBOd5bZ-TknOXJoRU"
+SESSION = "BQEO_1IAbkm1MUN3BccUYgDelw4oLgYa6w5bLmH5VRWZhRI41UX2BB11gAAuA1wGp3IjPvgAdc_SiC_8lAxG7llQ30xbt-mgCoy2L3KXXWJZz4RR8X-0gDLpNS170t944E6Eub3NFy3y_Vd1iISCGNn36X5_2Qjd-oK6wzYuoLFHcnLSFSWBOGnusPRUgI3YF7tQ7g0M36pzXeHNCf7Cw6NAVNyhDi4yZgKC6Wbd-97xxvbU_YmiqiOAa7Ee6QtBklt2hiNee_TxO5ISPcFgwGCyjhgu61xGiuNNELUtUzjiIUP84DFZyeVJq1CkWwk8iQCKj2dSbOwCLajA5mX7-pRzyYzk9QAAAAE_AG8rAA"
+FORCESUB = "DS_Botz"
+AUTH = "1125671241"
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
